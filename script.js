51b2d02b4c3931b0e038af168a8fd196e8a9e5be
Fatima Farooq <sanobernaz0312@gmail.com>
2024-08-29 20:12:46 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// index java
+function okFunction1() {
+    alert("You have successfully signed in with Google.");
+}
+
+// quiz java
+function okFunction2() {
+    alert("You have successfully completed the quiz.");
+}
+
+
